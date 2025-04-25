61c903b123ddb4f55be435b9a5ab62d5bb352f37
07sanad <07.sanad@gmail.com>
2025-04-25 01:25:23 +0100

    dev setup

---
+++ apiconnector/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <main className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Tailwind is working!
+        </h1>
+      </main>
+    );
+  }
+  
